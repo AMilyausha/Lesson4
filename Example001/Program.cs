025634aba66de1dc23b666fa6df66c33044e43e8
Amirova Milyausha <mila17.86@mail.ru>
2022-09-03 21:12:10 +0300

    Первое задание

---
+++ Example001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+int n=a;
+for (int i=1;i<b;i++)
+{
+    n = n*a;
+}
+ Console.WriteLine(n);
+
